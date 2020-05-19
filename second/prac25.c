4ad4d3e091a3235609300d82c6f6d8e9fe741a2b
yusuke <m1716@s.akashi.ac.jp>
2020-05-19 16:48:17 +0900

    m

--- second/prac25.c
+++ second/prac25.c
@@ -3,13 +3,11 @@
 int main(void){
     char a[20];
     char *p;
-
+    printf("文字を入力:");
     scanf("%s",a);    
     p = a;
-
     while(*p){
         printf("%s\n",p++);
     }
-
     return 0;
 }
